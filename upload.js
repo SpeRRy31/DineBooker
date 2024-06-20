05cac256149656976b92ca0d6ada93e76bff8a14
Олександр Біскупський <oleksandrb3136@gmail.com>
2024-06-20 17:59:05 +0300

    create tables realized save img on server

--- upload.js
+++ upload.js
@@ -1,15 +1,22 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
+
+// Перевірка існування каталогу і його створення
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+    destination: (req, file, cb) => {
+        cb(null, uploadDir);
     },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    filename: (req, file, cb) => {
+        cb(null, 'image-' + Date.now() + path.extname(file.originalname));
     }
 });
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage });
 
 module.exports = upload;
